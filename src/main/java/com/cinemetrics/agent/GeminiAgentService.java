4eede5b863e4d25fca2b2fb063bc46e30593ef50
Murali Nidugondi <muralinidugondi@Muralis-MacBook-Air.local>
2026-08-18 08:03:44 +0530

    Revert: simple VertexAI constructor, SDK 1.3.0

--- src/main/java/com/cinemetrics/agent/GeminiAgentService.java
+++ src/main/java/com/cinemetrics/agent/GeminiAgentService.java
@@ -67,23 +67,7 @@ public class GeminiAgentService {
         long start = System.currentTimeMillis();
         executedQueries.set(new ArrayList<>());
 
-        // Explicitly set credentials with correct scopes for Cloud Run
-        com.google.auth.oauth2.GoogleCredentials credentials;
-        try {
-            credentials = com.google.auth.oauth2.GoogleCredentials
-                .getApplicationDefault()
-                .createScoped(java.util.List.of(
-                    "https://www.googleapis.com/auth/cloud-platform"
-                ));
-        } catch (java.io.IOException e) {
-            throw new RuntimeException("Failed to get Google credentials: " + e.getMessage(), e);
-        }
-
-        try (VertexAI vertexAI = new VertexAI.Builder()
-                .setProjectId(projectId)
-                .setLocation(location)
-                .setCredentials(credentials)
-                .build()) {
+        try (VertexAI vertexAI = new VertexAI(projectId, location)) {
             GenerativeModel model = buildModel(vertexAI);
             ChatSession chat = model.startChat();
 
